f8c0824773db34bbf417eeaaba00da71844efa9d
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2021-08-16 12:33:04 -0500

    ver 0.1.12
    renamed performEnrichment.R to plumber.R to make more sense

--- startPlumberAPI.sh
+++ startPlumberAPI.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Start Tox21 Enricher R Plumber API
-R -e 'library(plumber); r <- plumb("./performEnrichment.R"); r$run(host="127.0.0.1",port=9000)'
+R -e 'library(plumber); r <- plumb("./plumber.R"); r$run(host="127.0.0.1",port=9000)'
