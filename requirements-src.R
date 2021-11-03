0bed332f37199120a9e703e32ca8c180f46cac9c
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2021-11-02 20:30:21 -0500

    ver 0.6.4
    - Adds support for tox21enricher/queue image for queue
    - Dockerfiles are now organized into Dockerfile.plumber, Dockerfile.queue, and Dockerfile.database
    - Updates requirements-src.R with missing dependencies - purrr library
    - Updated setup guide

--- requirements-src.R
+++ requirements-src.R
@@ -1,4 +1,4 @@
 pkgs <- c(
-    'config', 'future', 'ggplot2', 'httr', 'parallel', 'plyr', 'pool', 'promises', 'reticulate' ,'rjson', 'RPostgreSQL', 'stringr', 'tidyverse', 'uuid', 'xlsx'
+    'config', 'future', 'ggplot2', 'httr', 'parallel', 'plyr', 'pool', 'promises', 'purrr', 'reticulate' ,'rjson', 'RPostgreSQL', 'stringr', 'tidyverse', 'uuid', 'xlsx'
 )
 install.packages(pkgs, dependencies=TRUE, repos='https://cran.rstudio.com/')
