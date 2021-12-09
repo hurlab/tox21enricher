f22c0efb47fe3c1195a61402655d10d54c59fa09
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2021-12-08 20:04:34 -0600

    ver 0.8.3-hotfix
    adds missing reticulate dependency to requirements-src.R

--- requirements-src.R
+++ requirements-src.R
@@ -1,4 +1,4 @@
 pkgs <- c(
-    'config', 'DBI', 'future', 'ggplot2', 'httr', 'parallel', 'plyr', 'pool', 'promises', 'rjson', 'RPostgres', 'stringr', 'tidyverse', 'uuid'
+    'config', 'DBI', 'future', 'ggplot2', 'httr', 'parallel', 'plyr', 'pool', 'promises', 'reticulate', 'rjson', 'RPostgres', 'stringr', 'tidyverse', 'uuid'
 )
 install.packages(pkgs, dependencies=TRUE, repos='https://cran.rstudio.com/')
