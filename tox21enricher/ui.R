c896537fccb229b26deabcdee39eaf75e951573a
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2022-11-10 13:31:07 -0600

    ver 1.3.6
    ENHANCEMENTS
    - Updates manual.
    - Moves placement of checkboxes on splash page.
    - Adds hyperlink back to splash page on title page.

--- tox21enricher/ui.R
+++ tox21enricher/ui.R
@@ -289,7 +289,7 @@ shinyUI(function(){
         rclipboardSetup(),
         # Application title
         title="Tox21Enricher",
-        titlePanel("Tox21Enricher"),
+        titlePanel(HTML("<a href='/'>Tox21Enricher</a>")),
         # Sidebar with options
         sidebarLayout(
             sidebarPanel(
@@ -427,19 +427,19 @@ shinyUI(function(){
                         column(12,
                             uiOutput("inputInstructions"),
                         ),
+                        column(6,
+                               tipify(checkboxInput(inputId="includeChemsWithWarnings", label="Ignore chemicals with reactive structure warnings?", value=TRUE), title="A submitted chemical may contain a reactive functional group (nitrile, isocyanate, aldehyde, and/or epoxide) that does not appear in one or more similar chemicals. The opposite may also occur in which similar chemicals contain a reactive functional group not present in the submitted chemical. Checking this box will cause Tox21Enricher to ignore all similar chemicals that have a reactive functional group discrepancy with the submitted chemical. These chemicals will not be used in enrichment.", placement="top")
+                        ),
+                        column(6,
+                               tipify(checkboxInput(inputId="includeClustering", label="Perform clustering? (may take longer)", value=FALSE), title="Check this option to perform clustering. Enabling this may cause the enrichment process to take longer, depending on the size of the input.", placement="top")
+                        ),
                         column(12,
                             textAreaInput(inputId="submitted_chemicals", label=NULL, rows=12, width="100%", value="", resize="both"),
                         )
                     ),
-                    column(4,
+                    column(12,
                         actionButton(inputId="submit", "Submit", icon=icon("arrow-alt-circle-right"))
                     ), 
-                    column(4,
-                        tipify(checkboxInput(inputId="includeChemsWithWarnings", label="Ignore chemicals with reactive structure warnings?", value=TRUE), title="A submitted chemical may contain a reactive functional group (nitrile, isocyanate, aldehyde, and/or epoxide) that does not appear in one or more similar chemicals. The opposite may also occur in which similar chemicals contain a reactive functional group not present in the submitted chemical. Checking this box will cause Tox21Enricher to ignore all similar chemicals that have a reactive functional group discrepancy with the submitted chemical. These chemicals will not be used in enrichment.", placement="top")
-                    ),
-                    column(4,
-                        tipify(checkboxInput(inputId="includeClustering", label="Perform clustering? (may take longer)", value=TRUE), title="Check this option to perform clustering. Enabling this may cause the enrichment process to take longer, depending on the size of the input.", placement="top")
-                    )
                 ),
                 # Results page, hidden by default until request has completed
                 hidden(
