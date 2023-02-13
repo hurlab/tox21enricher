36157cb36f31957088be2e95657d3bd9710f00e8
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2023-02-13 09:25:59 -0600

    Removes database update & construction files
    These files are no longer necessary to include in the repo because we now host the entire database dumps on hurlab(dot)med(dot)und(dot)edu.

--- tox21enricher-database/update/TmpCommandToCalculateQValues.R
+++
@@ -1,8 +0,0 @@
-	
-	print("made it here first")
-	data = read.table (file="table_annoterm_pairwise_v2.1_pvalues.txt", sep="\t", header=TRUE)
-	qvalue = p.adjust (data$pvalue, method="fdr")
-	data = cbind (data, qvalue)
-	print("made it here second")
-	write.table (data, file="table_annoterm_pairwise_v2.1_pqvalues.txt", sep="\t", quote=FALSE)
-	
